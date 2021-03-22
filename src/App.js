edcc2f357e6041e6b989611a377b145b2b53708f
Aditya Sinha <72249021+adityawebdev@users.noreply.github.com>
2021-03-23 00:12:27 +0530

    name update

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
   }
   return (
     <div>
-      <h1 style={{ color: "orangered" }}>Pokedex</h1>
+      <h1 style={{ color: "orangered" }}>Pokelist</h1>
       <PokeList pokemon={pokemon} />
       <Pagination
         nextPage={nextPageUrl ? nextPage : null}
